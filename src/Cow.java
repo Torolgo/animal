e1883f77dd92cb51d1b40b5a5453e32ca00ee229
Torolgo <ylan.dessenne.spam@gmail.com>
2026-01-08 10:43:21 +0100

    feat: add TP1 part 1

---
+++ src/Cow.java
@@ -0,0 +1,10 @@
+public class Cow extends Animal {
+    public Cow(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meuh");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Cow cow = new Cow("Michelle", 12);
+        System.out.println(cow.name + " " + cow.age);
+        cow.makeSound();
+    }
+}
